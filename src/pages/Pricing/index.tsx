b3d0377afaaed42dee02e2a82ab3f644bf60bfbc
KauanMP <kauanmoreira1232@gmail.com>
2023-03-12 23:37:43 -0300

    Começando a criar os cards de preço

--- src/pages/Pricing/index.tsx
+++ src/pages/Pricing/index.tsx
@@ -1,10 +1,54 @@
-import { Container, TitleChamativo } from './styles'
+import { Container, H1Chamativo, H4Chamativo, Card} from './styles'
 
 const Pricing = () => {
   return (
     <Container>
-      <TitleChamativo>Contrate o melhor plano para você</TitleChamativo>
-      <h4>Temos planos para todos os portes, contrate oque melhor combina com sua biblioteca.</h4>
+      <H1Chamativo>Contrate o melhor plano para você</H1Chamativo>
+      <H4Chamativo>Temos planos para todos os portes, contrate oque melhor combina com sua biblioteca.</H4Chamativo>
+      <Card>
+        <div>
+          <ul>
+            <li>Plano Lite</li>
+            <li>Plano Simples</li>
+            <li>R$ 79 /mes</li>
+            <input type="button" value="Contratar pacote" />
+            <li>Vantagens do Plano Lite:</li>
+            <li>Sistema de emprestimo online com envio de notificação</li>
+            <li>Relatorios mensais</li>
+            <li>Controle dos usuarios da biblioteca</li>
+            <input type="button" value="Ler mais" />
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Plano Pro</li>
+            <li>Plano completo</li>
+            <li>R$ 79 /mes</li>
+            <input type="button" value="Contratar pacote" />
+            <li>Vantagens do Plano Pro:</li>
+            <li>Acesso ao acervo digital</li>
+            <li>Acesso a 3 telas para a dashboard</li>
+            <li>Controle dos usuarios da biblioteca</li>
+            <li>Sistema de emprestimo online com envio de notificação</li>
+            <li>Relatorios mensais</li>
+            <input type="button" value="Ler mais" />
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Plano Essentials</li>
+            <li>Plano padrão</li>
+            <li>R$ 109 /mes</li>
+            <input type="button" value="Contratar pacote" />
+            <li>Vantagens do Plano Essentials:</li>
+            <li>Acesso ao acervo digital</li>
+            <li>Controle dos usuarios da biblioteca</li>
+            <li>Sistema de emprestimo online com envio de notificação</li>
+            <li>Relatorios mensais</li>
+            <input type="button" value="Ler mais" />
+          </ul>
+        </div>
+      </Card>
     </Container>
   )
 }
--- src/pages/Pricing/styles.ts
+++ src/pages/Pricing/styles.ts
@@ -1,23 +1,37 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    position: relative;
-    width: auto;
+    position: absolute;
+    width: 100%;
     height: 40em;
 
     background: #335675;
 `
 
-export const TitleChamativo = styled.h1`
-    font-family: 'Poppins';
-    font-style: normal;
+export const H1Chamativo = styled.h1`
     font-weight: 600;
     font-size: 64px;
     line-height: 96px;
 
     display: flex;
-    align-items: center;
-    text-align: center;
+    justify-content: center;
 
     color: #FFFFFF;
+`
+
+export const H4Chamativo = styled.h4`
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 36px;
+
+    display: flex;
+    justify-content: center;
+
+    color: #F1F1F1;
+`
+
+export const Card = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
