5e5ff958ce472788e059cfa5c0908e183f0552f4
KauanMP <kauanmoreira1232@gmail.com>
2023-03-15 18:26:41 -0300

    Atualizando a pagina Pricing

--- src/pages/Pricing/index.tsx
+++ src/pages/Pricing/index.tsx
@@ -1,54 +1,54 @@
-import { Container, H1Chamativo, H4Chamativo, Card} from './styles'
+import { Container, H1Chamativo, H4Chamativo, Cards, CardLite, CardPro, CardEssentials, List, Linha, LiPrice, ButtonOne, ButtonTwo} from './styles'
 
 const Pricing = () => {
   return (
     <Container>
       <H1Chamativo>Contrate o melhor plano para você</H1Chamativo>
       <H4Chamativo>Temos planos para todos os portes, contrate oque melhor combina com sua biblioteca.</H4Chamativo>
-      <Card>
-        <div>
-          <ul>
-            <li>Plano Lite</li>
-            <li>Plano Simples</li>
-            <li>R$ 79 /mes</li>
-            <input type="button" value="Contratar pacote" />
-            <li>Vantagens do Plano Lite:</li>
-            <li>Sistema de emprestimo online com envio de notificação</li>
-            <li>Relatorios mensais</li>
-            <li>Controle dos usuarios da biblioteca</li>
-            <input type="button" value="Ler mais" />
-          </ul>
-        </div>
-        <div>
-          <ul>
-            <li>Plano Pro</li>
-            <li>Plano completo</li>
-            <li>R$ 79 /mes</li>
-            <input type="button" value="Contratar pacote" />
-            <li>Vantagens do Plano Pro:</li>
-            <li>Acesso ao acervo digital</li>
-            <li>Acesso a 3 telas para a dashboard</li>
-            <li>Controle dos usuarios da biblioteca</li>
-            <li>Sistema de emprestimo online com envio de notificação</li>
-            <li>Relatorios mensais</li>
-            <input type="button" value="Ler mais" />
-          </ul>
-        </div>
-        <div>
-          <ul>
-            <li>Plano Essentials</li>
-            <li>Plano padrão</li>
-            <li>R$ 109 /mes</li>
-            <input type="button" value="Contratar pacote" />
-            <li>Vantagens do Plano Essentials:</li>
-            <li>Acesso ao acervo digital</li>
-            <li>Controle dos usuarios da biblioteca</li>
-            <li>Sistema de emprestimo online com envio de notificação</li>
-            <li>Relatorios mensais</li>
-            <input type="button" value="Ler mais" />
-          </ul>
-        </div>
-      </Card>
+      <Cards>
+        <CardLite>
+          <List>
+            <Linha>Plano Lite</Linha>
+            <Linha>Plano Simples</Linha>
+            <LiPrice>R$ 79</LiPrice>
+            <ButtonOne type="button" value="Contratar pacote" />
+            <Linha>Vantagens do Plano Lite:</Linha>
+            <Linha>Sistema de emprestimo online com envio de notificação</Linha>
+            <Linha>Relatorios mensais</Linha>
+            <Linha>Controle dos usuarios da biblioteca</Linha>
+          </List>
+            <ButtonTwo type="button" value="Ler mais" />
+        </CardLite>
+        <CardPro>
+          <List>
+            <Linha>Plano Pro</Linha>
+            <Linha>Plano completo</Linha>
+            <LiPrice>R$ 159</LiPrice>
+            <ButtonOne type="button" value="Contratar pacote" />
+            <Linha>Vantagens do Plano Pro:</Linha>
+            <Linha>Acesso ao acervo digital</Linha>
+            <Linha>Acesso a 3 telas para a dashboard</Linha>
+            <Linha>Controle dos usuarios da biblioteca</Linha>
+            <Linha>Sistema de emprestimo online com envio de notificação</Linha>
+            <Linha>Relatorios mensais</Linha>
+          </List>
+            <ButtonTwo type="button" value="Ler mais" />
+        </CardPro>
+        <CardEssentials>
+          <List>
+            <Linha>Plano Essentials</Linha>
+            <Linha>Plano padrão</Linha>
+            <LiPrice>R$ 109</LiPrice>
+            <ButtonOne type="button" value="Contratar pacote" />
+            <Linha>Vantagens do Plano Essentials:</Linha>
+            <Linha>Acesso ao acervo digital</Linha>
+            <Linha>Controle dos usuarios da biblioteca</Linha>
+            <Linha>Sistema de emprestimo online com envio de notificação</Linha>
+            <Linha>Relatorios mensais</Linha>
+          </List>
+            <ButtonTwo type="button" value="Ler mais" />
+        </CardEssentials>
+      </Cards>
     </Container>
   )
 }
--- src/pages/Pricing/styles.ts
+++ src/pages/Pricing/styles.ts
@@ -3,9 +3,9 @@ import styled from "styled-components";
 export const Container = styled.div`
     position: absolute;
     width: 100%;
-    height: 40em;
+    height: 100%;
 
-    background: #335675;
+    background-image: linear-gradient( to bottom, #335675 65%, #61A9EB 35%);
 `
 
 export const H1Chamativo = styled.h1`
@@ -30,8 +30,75 @@ export const H4Chamativo = styled.h4`
     color: #F1F1F1;
 `
 
-export const Card = styled.div`
+export const Cards = styled.div`
     display: flex;
-    align-items: center;
     justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    margin-top: 5rem;
+`
+
+export const CardLite = styled.div`
+    background: #FFFFFF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 0.3rem;
+`
+
+export const CardPro = styled.div`
+    background: #FFFFFF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 0.3rem;
+    z-index: 1;
+`
+
+export const CardEssentials = styled.div`
+    background: #FFFFFF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 0.3rem;
+`
+
+export const List = styled.ul`
+    margin: 1rem 2rem 0rem 2rem;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    list-style-type: none;
+`
+
+export const Linha = styled.li`
+    padding: 0.6rem 0;
+    list-style-image: url("/assets/images/svg/Checking.svg");
+`
+
+export const LiPrice = styled.li`
+    font-weight: 400;
+    font-size: 3.7em;
+    color: #1A2E40;
+`
+
+export const ButtonOne = styled.input`
+    height: 2.75rem;
+    border-radius: 4px;
+    background-color: #1B8EF2;
+    color: #FFFFFF;
+    border: none;
+    cursor: pointer;
+
+    font-weight: 400;
+    font-size: 1.25em;
+    line-height: 100%;
+`
+
+export const ButtonTwo = styled.input`
+    width: 100%;
+    height: 3.5rem;
+    border-radius: 0px 0px 0.3rem 0.3rem;
+    background-color: #1B8EF2;
+    color: #FFFFFF;
+    border: none;
+    cursor: pointer;
+
+    font-weight: 400;
+    font-size: 1.25em;
+    line-height: 100%;
 `
