715de8bba0ecba6149d3cf3c593ddc948f71bf51
Gabriel Morais <gabe-morais@outlook.com>
2023-03-15 22:07:16 -0300

    Alterações register page

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,91 +1,37 @@
-import { Row } from "react-bootstrap";
-import {
-  Container,
-  OneRow,
-  BoxForm,
-  Titulo,
-  Botao,
-  Col,
-  Input,
-  Link,
-  TituloContainer,
-  TitleBox,
-  Spacing,
-  H2,
-  Info,
-  LabelFloat,
-  Body,
-  Backto,
-  PButton,
-  RegBox,
-  NameDiv,
-  Subtitle,
-  ImgBack,
-} from "./styles";
+import { useState } from "react";
+import FirstForm from "../../components/RegisterSection/FirstForm";
+import SecondForm from "../../components/RegisterSection/SecondForm";
+import { Container, Body, Backto, RegBox, ImgBack } from "./styles";
 
 function Register() {
+  let [firstName, setFirstName] = useState("");
+  let [lastName, setLastName] = useState("");
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  let [confirmPassword, setConfirmPassword] = useState("");
+  let [fisrtFormComplete, setFirstFormComplete] = useState(false);
+
+  console.log(firstName);
+
+  const handleFirstSubmit = () => {};
+
+  console.log(fisrtFormComplete);
+  const changeState = () => {
+    setFirstFormComplete(!fisrtFormComplete)
+  }
+
   return (
     <Container>
+      <button onClick={changeState}>É esse</button>
       <Backto href="/login">
-        <ImgBack src="public/assets/images/svg/backto.png" alt="" />
+        <ImgBack src="assets/images/svg/backto.png" alt="" />
       </Backto>
       <Body>
+      
         <RegBox>
-          <BoxForm>
-            {/* <TitleBox>
-            <Titulo>Registro</Titulo>
-          </TitleBox>*/}
-
-            <Col>
-              <H2>Criar uma conta na Bookshelf</H2>
-              <Info>
-                Para utilizar os serviços é preciso ter uma conta em nosso
-                sistema.
-              </Info>
-            </Col>
-            <form>
-              <LabelFloat>
-                <NameDiv>
-                  <div>
-                    <Subtitle>Nome</Subtitle>
-                    <Input type="text" name="nome" placeholder="" />
-                  </div>
-                  <Spacing />
-                  <div>
-                    <Subtitle>Sobrenome</Subtitle>
-                    <Input type="text" name="nome" placeholder="" />
-                  </div>
-                </NameDiv>
-              </LabelFloat>
-
-              <LabelFloat>
-                <Subtitle>E-mail</Subtitle>
-                <Input
-                  type="email"
-                  name="nome"
-                  placeholder="Informe seu email"
-                />
-              </LabelFloat>
-
-              <LabelFloat>
-                <Subtitle>Senha</Subtitle>
-                <Input
-                  type="password"
-                  name="senha"
-                  placeholder="Informe sua senha"
-                />
-              </LabelFloat>
-
-              <LabelFloat>
-                <Subtitle>Confirmar senha</Subtitle>
-                <Input type="password" name="confSenha" placeholder="" />
-              </LabelFloat>
-            </form>
-            <Info></Info>
-            <PButton>
-              <Botao type="submit" value="Enviar" />
-            </PButton>
-          </BoxForm>
+          {/* Componente 1 */}
+          {fisrtFormComplete ?  <FirstForm /> : <SecondForm />}
+          
         </RegBox>
       </Body>
     </Container>
