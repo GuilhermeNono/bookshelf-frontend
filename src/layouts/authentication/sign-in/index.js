113673cd7f698e63f97e89c16996d9bf0a9ba660
Gabe-Morais <gabe-morais@outlook.com>
2023-04-27 21:25:37 -0300

    Finalizando autênticação da página de login

--- src/layouts/authentication/sign-in/index.js
+++ src/layouts/authentication/sign-in/index.js
@@ -1,17 +1,17 @@
 /**
-=========================================================
-* Material Dashboard 2 React - v2.1.0
-=========================================================
+ =========================================================
+ * Material Dashboard 2 React - v2.1.0
+ =========================================================
 
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+ * Product Page: https://www.creative-tim.com/product/material-dashboard-react
+ * Copyright 2022 Creative Tim (https://www.creative-tim.com)
 
-Coded by www.creative-tim.com
+ Coded by www.creative-tim.com
 
  =========================================================
 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ */
 
 import { useState } from "react";
 
@@ -165,8 +165,14 @@ function Basic() {
             </MDBox>
 
             <MDBox mt={4} mb={1}>
-              <MDButton type="submit" variant="contained" color="info" fullWidth>
-                Entrar
+              <MDButton
+                type="submit"
+                variant="contained"
+                color="info"
+                disabled={authentication.loading}
+                fullWidth
+              >
+                {authentication.loading ? "Carregando" : "Entrar"}
               </MDButton>
             </MDBox>
 
